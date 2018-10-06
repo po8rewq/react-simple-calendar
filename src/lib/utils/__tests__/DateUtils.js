ce38d648c25bbbca6ebd579476041609f2d5eb4a
Adrien <adrien.fischer@gmail.com>
2018-10-06 13:18:42 +0100

    added unit tests for DateUtils

--- src/lib/utils/__tests__/DateUtils.js
+++ src/lib/utils/__tests__/DateUtils.js
@@ -33,7 +33,7 @@ describe('getDay', () => {
 describe('isToday', () => {
 	it('should always work for today', () => {
 		const now = new Date(Date.now());
-		const week = Math.floor(now.getDate() / 6);
+		const week = Math.floor(now.getDate() / 6) - 1;
 		const day = now.getDay() - 1;
 		expect(DateUtils.isToday(week, day, now)).toBe(true);
 	});
